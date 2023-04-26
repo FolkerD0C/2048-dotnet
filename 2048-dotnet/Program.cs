96cbd5d097cdf3eeb45b661e07f774dad32d5a07
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-04-26 23:28:38 +0200

    Update main file

--- 2048-dotnet/Program.cs
+++ 2048-dotnet/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Game2048;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello");
+    }
+}
