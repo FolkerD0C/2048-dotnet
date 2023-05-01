e408504cf5a3a234cce33a0318436935ed4c35ea
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-05-02 00:15:56 +0200

    Update Play.cs

--- 2048-dotnet/Play.cs
+++ 2048-dotnet/Play.cs
@@ -2,14 +2,79 @@ namespace Game2048;
 
 class Play
 {
+    LinkedList<GridInstance> undoChain;
+
+    Display display;
+
     public Play()
     {
-
+        undoChain = new LinkedList<GridInstance>();
+        Dictionary<int, (ConsoleColor Fg, ConsoleColor Bg)> colorSet = new Dictionary<int, (ConsoleColor Fg, ConsoleColor Bg)>();
+        ConsoleColor[] fgColors = new ConsoleColor[]
+        {
+            ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Red,
+            ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Red,
+            ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Red,
+            ConsoleColor.Black, ConsoleColor.Black, ConsoleColor.Black,  
+            ConsoleColor.White, ConsoleColor.White, ConsoleColor.White,
+            ConsoleColor.White, ConsoleColor.White
+        };
+        ConsoleColor[] bgColors = new ConsoleColor[]
+        {
+            ConsoleColor.Gray, ConsoleColor.Gray, ConsoleColor.Gray,
+            ConsoleColor.White, ConsoleColor.White, ConsoleColor.White,
+            ConsoleColor.DarkGray, ConsoleColor.DarkGray, ConsoleColor.DarkGray,
+            ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Red,
+            ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Red,
+            ConsoleColor.Red, ConsoleColor.Red
+        };
+        colorSet.Add(0, (ConsoleColor.White, ConsoleColor.Black));
+        for (int i = 0; i < fgColors.Length; i++)
+        {
+            colorSet.Add((int)Math.Pow(2, i + 1), (fgColors[i], bgColors[i]));
+        }
+        display = new Display(colorSet);
     }
 
     void HandleInput()
     {
-
+        MoveDirection? input = null;
+        switch(Console.ReadKey().Key)
+        {
+            case ConsoleKey.W: case ConsoleKey.UpArrow:
+                {
+                    input = MoveDirection.Up;
+                    break;
+                }
+            case ConsoleKey.S: case ConsoleKey.DownArrow:
+                {
+                    input = MoveDirection.Down;
+                    break;
+                }
+            case ConsoleKey.A: case ConsoleKey.LeftArrow:
+                {
+                    input = MoveDirection.Left;
+                    break;
+                }
+            case ConsoleKey.D: case ConsoleKey.RightArrow:
+                {
+                    input = MoveDirection.Right;
+                    break;
+                }
+            case ConsoleKey.Backspace:
+                {
+                    Undo();
+                    break;
+                }
+            case ConsoleKey.Escape:
+                {
+                    break;
+                }
+        }
+        if (input == null)
+        {
+            return;
+        }
     }
 
     void GameOver()
@@ -22,7 +87,17 @@ class Play
 
     }
 
-    void Run()
+    public void Run()
+    {
+
+    }
+
+    void Undo()
+    {
+
+    }
+
+    void Save()
     {
 
     }
