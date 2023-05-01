38ced70a90e37acac7212e4ffee90c7569311643
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-05-01 20:48:44 +0200

    Add Play.cs

---
+++ 2048-dotnet/Play.cs
@@ -0,0 +1,29 @@
+namespace Game2048;
+
+class Play
+{
+    public Play()
+    {
+
+    }
+
+    void HandleInput()
+    {
+
+    }
+
+    void GameOver()
+    {
+
+    }
+
+    void GameWon()
+    {
+
+    }
+
+    void Run()
+    {
+
+    }
+}
