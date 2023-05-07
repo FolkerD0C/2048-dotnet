f42c77f4efecef6cc6d0099da51eea6b65ccc97d
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-05-07 22:20:49 +0200

    Update resources, the place where the game runs

--- 2048-dotnet/Static/Resources.cs
+++ 2048-dotnet/Static/Resources.cs
@@ -8,20 +8,58 @@ public static class Resources
 {
     public static IFileHandler IOHandler { get; set; }
 
-    public static MainMenu EntryPoint { get; set; }
+    public static NavigationMenu EntryPoint { get; set; }
+
+    static string[] gameDescription = { "dummy" };
 
     static Resources()
     {
         IOHandler = new FileHandler();
     }
 
-    static void ConstructMenu()
+    static NavigationMenu ConstructMainMenu()
     {
+        List<IMenu> subMenus = new List<IMenu>();
 
+        subMenus.Add(new ObjectMenu("New game", Play.Initialize, IOHandler));
+
+        var allSaves = IOHandler.GetAllSaveFiles();
+        List<IMenu> loadGameSubMenus = allSaves.Select(tup =>
+            (IMenu)(new ObjectMenu(tup.FileName, Play.Initialize, IOHandler, tup.FullPath)
+            )).ToList();
+        loadGameSubMenus.Add(new ReturnMenu(MenuResult.Back));
+        var loadGameMenu = new NavigationMenu("Load game", loadGameSubMenus);
+        loadGameMenu.SetReturnValue(MenuResult.Obj);
+        subMenus.Add(loadGameMenu);
+
+        var jsonHighScores = IOHandler.GetSavedObject(IOHandler.HighscoresPath);
+        var highScores = IOHandler.Converter.DeserializeHighScores(jsonHighScores)
+            .Select(tup => tup.Name + $" {tup.Score}").ToArray();
+        subMenus.Add(new DisplayMenu("Highscores", highScores));
+
+        subMenus.Add(new DisplayMenu("Description", gameDescription));
+
+        subMenus.Add(new ActionMenu("Exit", GracefulExit));
+
+        return new NavigationMenu("mainMenu", subMenus);
     }
 
-    public static void Initialize()
+    public static void Run()
     {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.CursorVisible = false;
+        while (true)
+        {
+            var main = ConstructMainMenu();
+            main.MenuAction();
+        }
+    }
 
+    static void GracefulExit()
+    {
+        Console.Clear();
+        Console.CursorVisible = true;
+        Environment.Exit(0);
     }
 }
