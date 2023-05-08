548d22f296b13390bd0dec13e2e0b50b402b2d4b
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-05-08 23:06:06 +0200

    Refactor class so it can be a real prompt dialog

--- 2048-dotnet/Classes/Menus/PromptMenu.cs
+++ 2048-dotnet/Classes/Menus/PromptMenu.cs
@@ -6,10 +6,13 @@ public class PromptMenu : NavigationMenu
 {
     string[] promptMessage;
 
-    public PromptMenu(string displayName, string[] promptMessage, params ReturnMenu[] returnValues) :
-        base(displayName, returnValues.Select(r => (IMenu)r).Distinct().ToList())
+    Action action;
+
+    public PromptMenu(string displayName, string[] promptMessage, Action action) :
+        base(displayName, new List<IMenu>() { new ReturnMenu(MenuResult.Yes), new ReturnMenu(MenuResult.No) })
     {
         this.promptMessage = promptMessage;
+        this.action = action;
         MenuPosition = promptMessage.Length;
     }
 
@@ -33,43 +36,16 @@ public class PromptMenu : NavigationMenu
         Console.SetCursorPosition(0, MenuPosition);
     }
 
-    public MenuResult MenuAction()
+    public override MenuResult MenuAction()
     {
-        return Navigate();
-    }
-
-    protected override MenuResult Navigate()
-    {
-        MenuResult navigation = MenuResult.OK;
-        int cursorPosition = 0;
-        Func<int, int> moveCursor = x =>
-            x < 0 ? SubMenus.Count - 1 :
-            x >= SubMenus.Count ? 0 : x;
         DrawDisplayMessage();
-        while (navigation == MenuResult.OK)
-        {
-            DrawMenu(cursorPosition);
-            switch(HandleKeyboardInput())
-            {
-                case InputAction.Up:
-                    {
-                        cursorPosition = moveCursor(cursorPosition - 1);
-                        break;
-                    }
-                case InputAction.Down:
-                    {
-                        cursorPosition = moveCursor(cursorPosition + 1);
-                        break;
-                    }
-                case InputAction.Activate:
-                    {
-                        navigation = SubMenus[cursorPosition].MenuAction();
-                        break;
-                    }
-            }
-        }
-        ClearDisplay();
+        Navigate();
         ClearDisplayMessage();
-        return navigation;
+        var result = SubMenus[CursorPosition].MenuAction();
+        if (result == MenuResult.Yes)
+        {
+            action?.Invoke();
+        }
+        return result;
     }
 }
