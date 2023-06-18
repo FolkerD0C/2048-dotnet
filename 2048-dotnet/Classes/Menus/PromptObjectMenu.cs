1754e94e00626a676b808cef9251fff8d0bf0989
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-06-18 22:16:38 +0200

    Add new menu type

---
+++ 2048-dotnet/Classes/Menus/PromptObjectMenu.cs
@@ -0,0 +1,44 @@
+using Game2048.Interfaces;
+using Game2048.Static;
+
+namespace Game2048.Classes.Menus;
+
+public class PromptObjectMenu : NavigationMenu
+{
+    string[] promptMessage;
+
+    Action<object[]> action;
+
+    object[] args;
+
+    public PromptObjectMenu(string displayName, IEnumerable<string> promptMessage, Action<object[]> action, object[] args) :
+        base(displayName, new List<IMenu>() { new ReturnMenu(MenuResult.Yes), new ReturnMenu(MenuResult.No) })
+    {
+        this.promptMessage = promptMessage.ToArray();
+        this.action = action;
+        this.args = args;
+        MenuPosition = promptMessage.Count();
+    }
+
+    void DrawDisplayMessage()
+    {
+        for (int i = 0; i < promptMessage.Length; i++)
+        {
+            Display.PrintText(promptMessage[i], 0, i, ConsoleColor.White, ConsoleColor.Black);
+        }
+    }
+
+    public override MenuResult MenuAction()
+    {
+        Display.NewLayout();
+        DrawDisplayMessage();
+        Navigate();
+        var result = SubMenus[CursorPosition].MenuAction();
+        if (result == MenuResult.Yes)
+        {
+            action?.Invoke(args);
+        }
+        Display.PreviousLayout();
+        return result;
+    }
+}
