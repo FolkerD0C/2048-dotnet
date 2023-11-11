fbaa72f50c6f9403ea41bc15bab3b4a2f5fcc9dc
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-11 21:23:11 +0100

    Add game model

---
+++ 2048-dotnet-backend/Models/Interfaces/IGamePosition.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using Game2048.Backend.Helpers.Enums;
+
+namespace Game2048.Backend.Models;
+
+public interface IGamePosition : ISerializable
+{
+    public IList<IList<int>> Grid { get; }
+    public int Score { get; }
+    public bool CanMove { get; }
+    public void Move(MoveDirection direction);
+    public IGamePosition Copy();
+}
