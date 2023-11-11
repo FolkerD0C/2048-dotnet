b89dab23de8f5708390fda93295292ffad472477
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-11 21:23:11 +0100

    Move GameConfiguration

--- 2048-dotnet-backend/Helpers/Config/GameConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace Game2048.Backend.Helpers.Config;
-
-public static class GameConfiguration
-{
-    public static int MaxHighscoresListLength = 10;
-
-    public static string? GameDataDirectory = null;
-}
