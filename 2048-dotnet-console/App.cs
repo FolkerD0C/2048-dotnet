60332dd54e4757baef9e7f1b2ab6d0dc367c6efa
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-12 19:46:27 +0100

    Misc changes that don't have effect yet

---
+++ 2048-dotnet-console/App.cs
@@ -0,0 +1,12 @@
+﻿using Game2048.Backend.Logic;
+
+namespace Game2048.ConsoleFrontend;
+
+class App
+{
+    void Main(string[] args)
+    {
+        var logic = new GameLogic();
+        logic.NewGame();
+    }
+}
--- 2048-dotnet-console/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Game2048.Console;
-
-class ConsoleApp
-{
-    void Main(string[] args)
-    {
-        
-    }
-}
