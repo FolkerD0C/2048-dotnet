5ab04339e03636578bbe0b44f4d24c7a7a9c1bb0
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-16 00:18:26 +0100

    Add ConfigLogic for handling config over the logic layer
    (cherry picked from commit 9dd862bbee5acc5ba5184815f7b401a20a8e17b6)

---
+++ Game2048.Logic/Classes/ConfigLogic.cs
@@ -0,0 +1,40 @@
+﻿using Game2048.Config;
+using Game2048.Shared.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game2048.Logic;
+
+public class ConfigLogic : IConfigLogic
+{
+    public IEnumerable<ConfigItem> GetConfigItems()
+    {
+        var items = ConfigManager.GetConfigItems();
+        return items.Select(item => new ConfigItem()
+        {
+            Name = item.Name,
+            Value = item.Value,
+            Type = item.Type
+        });
+    }
+
+    public T GetConfigValue<T>(string configItemName, T configItemType)
+    {
+        return ConfigManager.GetConfigItem(configItemName, configItemType);
+    }
+
+    public void LoadConfig()
+    {
+        ConfigManager.Load();
+    }
+
+    public void SaveConfig()
+    {
+        ConfigManager.Save();
+    }
+
+    public void SetConfigValue<T>(string configItemName, T newValue)
+    {
+        ConfigManager.SetConfigItem(configItemName, newValue);
+    }
+}
