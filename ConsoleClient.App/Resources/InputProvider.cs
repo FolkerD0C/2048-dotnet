1c78e21ba0bfd585756387cc7236b3e09c9d63aa
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-22 00:31:03 +0100

    Add input providers for the console client

---
+++ ConsoleClient.App/Resources/InputProvider.cs
@@ -0,0 +1,81 @@
+﻿using ConsoleClient.Menu.Enums;
+using Game2048.Shared.Enums;
+using System;
+
+namespace ConsoleClient.App.Resources;
+
+internal static class InputProvider
+{
+    internal static GameInput ProvidePlayInput()
+    {
+        GameInput input = GameInput.Unknown;
+        while (input == GameInput.Unknown)
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    {
+                        return GameInput.Up;
+                    }
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    {
+                        return GameInput.Down;
+                    }
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    {
+                        return GameInput.Left;
+                    }
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    {
+                        return GameInput.Right;
+                    }
+                case ConsoleKey.Backspace:
+                case ConsoleKey.U:
+                    {
+                        return GameInput.Undo;
+                    }
+                case ConsoleKey.P:
+                case ConsoleKey.Escape:
+                    {
+                        return GameInput.Pause;
+                    }
+                default:
+                    break;
+            }
+        }
+        throw new InvalidOperationException("Invalid input.");
+    }
+
+    internal static MenuInput ProvideMenuInput()
+    {
+        MenuInput input = MenuInput.Unknown;
+        while (input == MenuInput.Unknown)
+        {
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    {
+                        return MenuInput.Up;
+                    }
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    {
+                        return MenuInput.Down;
+                    }
+                case ConsoleKey.Enter:
+                case ConsoleKey.Spacebar:
+                    {
+                        return MenuInput.Select;
+                    }
+                default:
+                    break;
+            }
+        }
+        throw new InvalidOperationException("Invalid input.");
+    }
+}
