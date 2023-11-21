df7568c0c4c8fd6e45792b384782b33c4de113c4
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-22 00:25:41 +0100

    Add extension for strings

---
+++ ConsoleClient.App/Helpers/Static/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleClient.App.Helpers;
+
+internal static class Extensions
+{
+    // TODO make it slice on words
+    internal static IList<string> Slice(this string input, int length)
+    {
+        IList<string> result = new List<string>();
+        while (input.Length >= length)
+        {
+            result.Add(input[..length]);
+            input = input[length..];
+        }
+        result.Add(input);
+        return result;
+    }
+}
