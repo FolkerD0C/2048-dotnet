f5dde8d49c60e91d61bea75b92ef8f07fbbb8e53
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-24 08:06:02 +0100

    Update todo

--- ConsoleClient.App/Helpers/Static/Extensions.cs
+++ ConsoleClient.App/Helpers/Static/Extensions.cs
@@ -4,7 +4,7 @@ namespace ConsoleClient.App.Helpers;
 
 internal static class Extensions
 {
-    // TODO make it slice on words
+    // TODO make it split on words and move it to ConsoleClient.AppUI.Static
     internal static IList<string> Slice(this string input, int length)
     {
         IList<string> result = new List<string>();
