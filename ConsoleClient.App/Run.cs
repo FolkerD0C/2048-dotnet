b588f86e75eb6362152b441dd2703527217d1bf8
FolkerD0C <104650641+FolkerD0C@users.noreply.github.com>
2023-11-26 16:59:22 +0100

    Modify main to accept command line args

--- ConsoleClient.App/Run.cs
+++ ConsoleClient.App/Run.cs
@@ -1,9 +1,44 @@
-﻿namespace ConsoleClient.App;
+﻿using CommandLine;
+using ConsoleClient.App.Configuration;
+using ConsoleClient.App.Configuration.Enums;
+using ConsoleClient.AppUI.Enums;
+using ConsoleClient.AppUI.Misc;
+using System;
+
+namespace ConsoleClient.App;
 
 class Run
 {
     static void Main(string[] args)
     {
-        AppEnvironment.MainMenu.Navigate();
+        AppEnvironment.ConfigLogic.LoadConfig();
+        var optionsParser = Parser.Default.ParseArguments<ConfigOptions>(args);
+        var setConfigResult = ConfigOptionParser.SetConfigOptions(optionsParser);
+        switch (setConfigResult.Result)
+        {
+            case ConfigurationResultType.Failure:
+                {
+                    new MessageOverlay(setConfigResult.Message ?? "", MessageType.Error).PrintMessage();
+                    break;
+                }
+            case ConfigurationResultType.Success:
+                {
+                    try
+                    {
+                        AppEnvironment.MainMenu.Navigate();
+                        if (optionsParser.Value.SaveConfig)
+                        {
+                            AppEnvironment.ConfigLogic.SaveConfig();
+                        }
+                    }
+                    catch (Exception exc)
+                    {
+                        new MessageOverlay(exc.Message, MessageType.Error).PrintMessage();
+                    }
+                    break;
+                }
+            default:
+                break;
+        }
     }
 }
